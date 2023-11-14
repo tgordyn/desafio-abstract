bd6b34758984d4903167962aa3f1e03f9dda695a
Zequi Zavala <ezequielzavala@MacBook-Pro-2.local>
2023-11-14 10:44:40 -0300

    chore: limpieza

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,22 +1,7 @@
 import "@/styles/globals.css";
-import {
-  ChakraProvider,
-  Box,
-  GridItem,
-  Image,
-  Text,
-  Grid,
-  Badge,
-} from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 export default function App({ Component, pageProps }) {
-  const pokemonData = {
-    name: "Pikachu",
-    number: "#025",
-    type: "Electric",
-    image: "pikachu_image_url.jpg",
-  };
-
   return (
     <ChakraProvider>
       <Component {...pageProps} />
